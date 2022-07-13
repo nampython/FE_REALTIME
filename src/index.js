266cc40d214881edaee7e825f75916eecb8ecb89
nam.dinh <nam.dinh@terralogic.com>
2022-07-13 10:11:17 +0700

    add Live Column Charts

--- src/index.js
+++ src/index.js
@@ -35,12 +35,6 @@ const kafka = require('kafka-node'),
         }
     );
 
-
-
-
-
-
-
 // database
 const db = require('./config/db/mongdb');
 // connect to db
@@ -60,18 +54,7 @@ app.set('views', path.join(__dirname, 'resources/views'));
 const route = require('./resources/routes')
 route(app);
 
-
-
-
 const tweet = require('C:/Users/nam.dinh/Desktop/study/big/src/resources/app/models/Sentiment.js')
-
-// tweet.watch().on('change',(change)=>{
-//     io.on('connection', (client) => {
-//         console.log('push sucessfully')
-//         client.emit('tweets', change.fullDocument);
-//     })
-//     console.log(change.fullDocument);
-// })
 
 io.on('connection', (client) => {
     consumer.on('message', function (message) {
@@ -79,12 +62,58 @@ io.on('connection', (client) => {
     });
 })
 io.on('connection', (client) => {
+    var sentimentPositives = 0;
+    var sentimentNegatives = 0;
+    var sentimentNeutral = 0;
     tweet.watch().on('change', (change) => {
-        console.log('push sucessfully')
+        if (change.fullDocument.sentimentType == 'Neutral') {
+            sentimentNeutral += 1;
+        }
+        if (change.fullDocument.sentimentType == 'Positive') {
+            sentimentPositives += 1;
+        }
+        if (change.fullDocument.sentimentType == 'Negative') {
+            sentimentNegatives += 1;
+        }
         client.emit('tweets', change.fullDocument);
+        console.log(`${sentimentPositives}:${sentimentNegatives}:${sentimentNeutral}`);
+        client.emit('tweets2', {
+            'Positive' : sentimentPositives,
+            'Negative': sentimentNegatives,
+            'Neutral': sentimentNeutral
+        })
     })
 })
 
+
+// {
+//     _id: {
+//       _data: '8262CBF15E000000532B022C0100296E5A1004FFEBD402A7E44C66B454BF567F0AFAAD46645F6964006462CBF1B7B9D5C076D91C52060004'
+//     },
+//     operationType: 'insert',
+//     clusterTime: new Timestamp({ t: 1657532766, i: 83 }),
+//     fullDocument: {
+//       _id: new ObjectId("62cbf1b7b9d5c076d91c5206"),
+//       id: new Long("1546430579821858800"),
+//       timeStamp: 'Some(1657532756177)',
+//       tweet64: '98888!858888885 9888!858888885 887989\n' +
+//         '172022  8888189887988\n' +
+//         '272022  8989887988989\t8\n' +
+//         '1072022  988988849\t8\n' +
+//         '2022A9PAET\n' +
+//         '\n' +
+//         '9516 8\x0B888828!883988 201 8!858888885 ',
+//       screen_name: 'bkkchangelog',
+//       url_img: 'http://abs.twimg.com/sticky/default_profile_images/default_profile_normal.png',
+//       latitude: 13.81067591,
+//       longitude: 100.73413141,
+//       score: 0,
+//       lang: 'th',
+//       sentimentType: 'Neutral'
+//     },
+//     ns: { db: 'M001', coll: 'tweets' },
+//     documentKey: { _id: new ObjectId("62cbf1b7b9d5c076d91c5206") }
+//   }
 
 const pipeline = [{
     $group: {
